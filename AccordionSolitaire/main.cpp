e3c53c83446a2534239256d99dceb2d0f41d469d
Jacob Speicher <speicherjacob@gmail.com>
2022-05-11 17:14:34 -0500

    clean up main from old text

--- AccordionSolitaire/main.cpp
+++ AccordionSolitaire/main.cpp
@@ -18,16 +18,6 @@ sf::Shader cardShader;
 sf::Shader deckShader;
 sf::Shader stackShader;
 
-//sf::Font font; 
-//
-//sf::Text title;
-//sf::Text subtitle;
-//sf::Text score;
-//sf::Text cardsLeft;
-//sf::Text resetText;
-//sf::Text gameOver;
-//sf::Text version;
-
 static bool settled = false;
 
 double deltaTime = 0.0;
@@ -47,38 +37,6 @@ int main()
 	if (!cardShader.loadFromFile("card.vert", "dull.frag")) {}
 	if (!deckShader.loadFromFile("card.vert", "dull.frag")) {}
 	if (!stackShader.loadFromFile("card.vert", "bright.frag")) {}
-
-	/*if (!font.loadFromFile("Assets/Fonts/Accordion.otf")) {}
-
-	title.setFont(font);
-	subtitle.setFont(font);
-	score.setFont(font);
-	cardsLeft.setFont(font);
-	resetText.setFont(font);
-	gameOver.setFont(font);
-	version.setFont(font);
-
-	title.setString("Accordion Solitaire");
-	subtitle.setString("by Jacob Speicher");
-	resetText.setString("R to Reset");
-	gameOver.setString("Game Over");
-	version.setString("v1.3.1");
-
-	title.setCharacterSize(22);
-	subtitle.setCharacterSize(18);
-	score.setCharacterSize(18);
-	cardsLeft.setCharacterSize(18);
-	resetText.setCharacterSize(18);
-	gameOver.setCharacterSize(22);
-	version.setCharacterSize(10);
-
-	title.setPosition(sf::Vector2f(250, 900));
-	subtitle.setPosition(sf::Vector2f(300, 950));
-	score.setPosition(sf::Vector2f(1500, 900));
-	cardsLeft.setPosition(sf::Vector2f(1500, 950));
-	resetText.setPosition(sf::Vector2f(1500, 1000));
-	gameOver.setPosition(sf::Vector2f(1200, 900));
-	version.setPosition(sf::Vector2f(20, 1050));*/
 
 	sf::Clock clock;
 	int gameOverCheckInterval = 0;
@@ -107,22 +65,8 @@ int main()
 
 		window.clear(sf::Color::Color(0, 120, 0));
 
-		/*if (!started)
-		{
-			window.draw(title);
-			window.draw(subtitle);
-			window.draw(version);
-		}*/
-
 		if (started)
 		{
-			//window.draw(resetText);
-
-			/*score.setString("Score : " + std::to_string(game.getScore()));
-			window.draw(score);
-
-			cardsLeft.setString("Cards Left : " + std::to_string(game.deck->cards.size()));
-			window.draw(cardsLeft);*/
 			gameUI->Draw();
 
 			gameScene->Draw();
@@ -137,7 +81,6 @@ int main()
 					}
 					settled = true;
 					game.DrawGameOverScreen(window);
-					//window.draw(gameOver);
 				}
 				else
 				{
