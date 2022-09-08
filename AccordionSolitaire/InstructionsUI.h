586ffdce4bd0e97e8ab0eac759df6ce392dfb826
Jacob Speicher <speicherjacob@gmail.com>
2022-09-07 23:33:08 -0500

    make instructions screen

--- AccordionSolitaire/InstructionsUI.h
+++ AccordionSolitaire/InstructionsUI.h
@@ -18,9 +18,16 @@ public:
 #pragma endregion UserInterfaceRequired
 
 private:
+    sf::Texture deckTexture;
+    sf::Sprite deckPile;
+
+    sf::Texture arrowTexture;
+    sf::Sprite arrow;
+
     Card* ace_clubs = new Card(1, 0);
     Card* ace_diamonds = new Card(1, 1);
     Card* jack_spades = new Card(11, 3);
-    Card* five_hearts = new Card(5, 2);
+    Card* five_spades = new Card(5, 3);
+    Card* blank_card = new Card(0, 0);
 };
 
